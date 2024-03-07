65e93ecb712fd55f1dfaaaa330f6d19003d885bc
Andrei Debono <andrei0427@gmail.com>
2024-03-07 20:53:45 +0100

    home page link

--- view/pages/index_templ.go
+++ view/pages/index_templ.go
@@ -63,7 +63,7 @@ func Index(props HomeEntity) templ.Component {
 			var templ_7745c5c3_Var3 string
 			templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinStringErrs(props.CTA)
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `view/pages/index.templ`, Line: 54, Col: 16}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `view/pages/index.templ`, Line: 55, Col: 16}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var3))
 			if templ_7745c5c3_Err != nil {
